ac4f217419ac4075715efbb91d1bbfb63969fef4
teivaz <github@teivaz.com>
2022-08-29 19:46:35 +0200

    Bumped version of kicad

--- electronics/scripts/dependencies.sh
+++ electronics/scripts/dependencies.sh
@@ -10,7 +10,7 @@ set -v
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo add-apt-repository --yes ppa:kicad/kicad-5.1-releases
+sudo add-apt-repository --yes ppa:kicad/kicad-6.0-releases
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y kicad inkscape poppler-utils xdotool recordmydesktop python3-dev python3-pip
 
